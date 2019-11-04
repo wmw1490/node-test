6168c867a66407a6a67889383136771ea2eee7c1
wmw1490 <wmw1490@yahoo.com>
2019-11-03 21:08:00 -0500

    fixed vars

--- node-test/lambda.js
+++ node-test/lambda.js
@@ -1,10 +1,10 @@
 exports.handler = function(event, context, callback) {
     
-    let bucketname = 's3-bucket-ww'
-    let fname = 's3-fname'
+    let bucketName = 's3-bucket-ww'
+    let fileName = 's3-fname'
     let fsize = 3409008
     let bsize = 4096
-    blocks = fsize/bsize
-    console.log('Node-testing.....' + bucketname + '/' + fname + ' Blocks: ' + blocks)
+    let blocks = fsize/bsize
+    console.log('Node-testing.....' + bucketName + '/' + fileName + ' Blocks: ' + blocks)
     callback(null, {"message": "Successfully executed"});
 }
