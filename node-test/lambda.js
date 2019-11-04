ac5bdf467d105b4ec698ce5df1031050dfb441c3
wmw1490 <wmw1490@yahoo.com>
2019-11-03 20:39:03 -0500

    mod vars

--- node-test/lambda.js
+++ node-test/lambda.js
@@ -2,6 +2,6 @@ exports.handler = function(event, context, callback) {
     
     let bucketname = 's3-bucket-ww'
     let fname = 's3-fname'
-    console.log('Node-testing.....' + bucketname )
+    console.log('Node-testing.....' + bucketname + '/' + fname)
     callback(null, {"message": "Successfully executed"});
 }
